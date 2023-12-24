95357bce966204790212b3f8dd1ee89688a97c5a
Lucas Pinheiro <pinheiro.lucasaugusto@gmail.com>
2023-12-24 10:09:31 -0300

    2023 - 24 - part 2

--- 2023/24/solution.go
+++ 2023/24/solution.go
@@ -1,25 +1,28 @@
 package pkg202324
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
-type Line struct {
+type stone struct {
 	p Point
 	v Velocity
 }
 
-func (l *Line) gradient() float64 {
+func (l *stone) linegradient() float64 {
 	other := Point{x: l.p.x + l.v.vx, y: l.p.y + l.v.vy}
 	return float64(other.y-l.p.y) / float64(other.x-l.p.x)
 }
 
-func (l *Line) yintersect() float64 {
+func (l *stone) yintersect() float64 {
 	// y = gradient*x + yintersect
 	y := float64(l.p.y)
-	gradient := l.gradient()
+	gradient := l.linegradient()
 	x := float64(l.p.x)
 
 	return y - (gradient * x)
@@ -32,9 +35,9 @@ func (l *Line) yintersect() float64 {
 // Then, the intersection will bappen at:
 // x = (b1*c2 - b2*c1) / (a1*b2 - a2*b1)
 // y = (c1*a2 - c2*a1) / (a1*b2 - a2*b1)
-func (l *Line) intersect(other Line, min, max float64) bool {
-	a1 := l.gradient()
-	a2 := other.gradient()
+func (l *stone) intersect(other stone, min, max float64) bool {
+	a1 := l.linegradient()
+	a2 := other.linegradient()
 	b1 := float64(-1)
 	b2 := float64(-1)
 	c1 := l.yintersect()
@@ -78,12 +81,12 @@ func A(input string, min, max float64) int {
 		panic(err)
 	}
 
-	lines := parseLines(string(d))
+	stones := parseStones(string(d))
 
 	intersections := 0
-	for i := 0; i < len(lines); i++ {
-		for j := i + 1; j < len(lines); j++ {
-			if lines[i].intersect(lines[j], min, max) {
+	for i := 0; i < len(stones); i++ {
+		for j := i + 1; j < len(stones); j++ {
+			if stones[i].intersect(stones[j], min, max) {
 				intersections++
 			}
 		}
@@ -92,19 +95,105 @@ func A(input string, min, max float64) int {
 	return intersections
 }
 
-func parseLines(data string) []Line {
-	lines := []Line{}
+// NOTE: this requires sagemath (https://www.sagemath.org) to be installed and available in the path.
+func B(input string) int64 {
+	d, err := os.ReadFile(input)
+	if err != nil {
+		panic(err)
+	}
+
+	stones := parseStones(string(d))
+
+	// I'm guessing that if I find when my throw collapses with the first N blocks, I find it for everything.
+	// I'm using 3 here, but any value >3 works.
+	// I found 3 by experimenting, which didn't took long:
+	// - 1 couldn't be used
+	// - 2 didn't give a proper answer
+	numStones := 3
+
+	vars := []string{"x", "y", "z", "vx", "vy", "vz"}
+	for i := 0; i < numStones; i++ {
+		vars = append(vars, fmt.Sprintf("t%d", i+1))
+	}
+
+	equations := []string{}
+	expressions := ""
+	for i := 0; i < numStones; i++ {
+		stone := stones[i]
+		expressions += fmt.Sprintf("eq%d = x + (vx * t%d) == %d + (%d * t%d)\n", (i*numStones)+1, i+1, stone.p.x, stone.v.vx, i+1)
+		expressions += fmt.Sprintf("eq%d = y + (vy * t%d) == %d + (%d * t%d)\n", (i*numStones)+2, i+1, stone.p.y, stone.v.vy, i+1)
+		expressions += fmt.Sprintf("eq%d = z + (vz * t%d) == %d + (%d * t%d)\n", (i*numStones)+3, i+1, stone.p.z, stone.v.vz, i+1)
+		equations = append(equations, []string{
+			fmt.Sprintf("eq%d", (i*numStones)+1),
+			fmt.Sprintf("eq%d", (i*numStones)+2),
+			fmt.Sprintf("eq%d", (i*numStones)+3),
+		}...)
+	}
+
+	script := ""
+	script += "var ('" + strings.Join(vars, " ") + "')\n"
+	script += expressions
+	script += "print(solve([" + strings.Join(equations, ",") + "]," + strings.Join(vars, ",") + "))"
+
+	f, err := os.CreateTemp("", "*.sage")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Expression: %s\n", script)
+
+	_, err = f.Write([]byte(script))
+	if err != nil {
+		panic(err)
+	}
+
+	f.Close()
+	defer os.Remove(f.Name())
+
+	out, err := exec.Command("sage", f.Name()).Output()
+	if err != nil {
+		panic(fmt.Errorf("error executing sage: %v", err))
+	}
+
+	fmt.Printf("Result: %s", out)
+
+	regex := regexp.MustCompile(`x == (\d+), y == (\d+), z == (\d+)*`)
+	matches := regex.FindStringSubmatch(string(out))
+	if len(matches) == 0 {
+		panic("no matches")
+	}
+
+	x, err := strconv.ParseInt(matches[1], 10, 64)
+	if err != nil {
+		panic(fmt.Errorf("error parsing result: %v", err))
+	}
+
+	y, err := strconv.ParseInt(matches[2], 10, 64)
+	if err != nil {
+		panic(fmt.Errorf("error parsing result: %v", err))
+	}
+
+	z, err := strconv.ParseInt(matches[3], 10, 64)
+	if err != nil {
+		panic(fmt.Errorf("error parsing result: %v", err))
+	}
+
+	return x + y + z
+}
+
+func parseStones(data string) []stone {
+	stones := []stone{}
 	for _, line := range strings.Split(data, "\n") {
 		pAndV := strings.Split(line, " @ ")
 		ps := parseCommaSeparatedIntList(pAndV[0])
 		vs := parseCommaSeparatedIntList(pAndV[1])
-		lines = append(lines, Line{
+		stones = append(stones, stone{
 			p: Point{x: ps[0], y: ps[1], z: ps[2]},
 			v: Velocity{vx: vs[0], vy: vs[1], vz: vs[2]},
 		})
 	}
 
-	return lines
+	return stones
 }
 
 func parseCommaSeparatedIntList(s string) []int {
--- 2023/24/solution_test.go
+++ 2023/24/solution_test.go
@@ -8,5 +8,10 @@ import (
 
 func Test__SolutionA(t *testing.T) {
 	require.Equal(t, 2, A("example.txt", 7.0, 27.0))
-	require.Equal(t, 2, A("input.txt", 200000000000000.0, 400000000000000.0))
+	require.Equal(t, 31921, A("input.txt", 200000000000000.0, 400000000000000.0))
+}
+
+func Test__SolutionB(t *testing.T) {
+	require.Equal(t, int64(47), B("example.txt"))
+	require.Equal(t, int64(761691907059631), B("input.txt"))
 }
